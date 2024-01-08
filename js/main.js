6ff930b6d723e9a3cde6713ffded628257c7b069
Hjname <whj9910@gmail.com>
2024-01-08 11:29:02 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  // Logic..
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -155,6 +135,3 @@ function floatingObject (selector, delay, size) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
  });
-
- const thisYear = document.querySelector('.this-year');
- thisYear.textContent = new Date().getFullYear(); // 해당연도의 숫자가 this-year에 들어간다
